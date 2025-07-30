6d8b7b6f0f24aa2689481c7596f4ffcb5dbc73b2
du_autodl <zane.d11@outlook.com>
2025-07-31 00:10:03 +0800

    update

--- run.py
+++
@@ -1,6 +0,0 @@
-import os
-
-# scannetpp_scenes = ['8b5caf3398', '116456116b', '13c3e046d7', '21d970d8de']
-scannetpp_scenes = ['116456116b']
-for scene in scannetpp_scenes:
-    os.system(f"python render.py --skip_mesh -m ./exps/experiments_v0/scannetpp-2025-02-14_01-00-30/{scene} --depth_ratio 1.0")
